ce45ee01b1efa4b995eb97ab2838c0fda7851b04
Jared Bowman <jared.bowman@bsu.edu>
2023-10-25 20:06:48 -0400

    Add signal for inventory scene

--- project/Objects/object.gd
+++ project/Objects/object.gd
@@ -1,8 +1,24 @@
-extends Control
+extends TextureButton
+
+
+signal clicked
+
+
+@onready var inventory : Inventory
 
 @export var clickable_name : String
+@export var clickable_sprite : Texture
+
+
+func _ready():
+	texture_normal = clickable_sprite
 
 
 func _on_pressed():
 	print("Pressed " + clickable_name)
+	clicked.emit()
 	queue_free()
+
+
+func _on_clicked():
+	inventory.add_item(clickable_name, clickable_sprite.resource_path)
