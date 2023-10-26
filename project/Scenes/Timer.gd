3a170872f4fb1d62eec9be1e1cc5ff623cfa07da
Jack Carthew <jdcarthew@bsu.edu>
2023-10-25 20:56:36 -0700

    Add timer functionality

--- project/Scenes/Timer.gd
+++ project/Scenes/Timer.gd
@@ -1,8 +1,7 @@
 extends Timer
 
 
-var ms = 0
-var s = 0
+var timePassed = 0;
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,15 +11,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if ms > 9:
-		s += 1
-		ms = 0
-	
-	$Label.text = str(s)
-	print(str(s))
-	
+	pass
 
 func _on_timeout():
-	ms += 1
-	
-	pass # Replace with function body.
+	timePassed += 1
+	$Label.text = "Time: " + str(timePassed)
