ea042e2801e6c193d5726e7bbd3b3c1d00217246
Justyn Fox <jjfox@bsu.edu>
2023-11-07 14:51:33 -0500

    Add room states
    instantiate and delete minigames, and give room "active" and "inactive" state, so that room is not reloaded.

--- project/Objects/New_Scene_Object.gd
+++ project/Objects/New_Scene_Object.gd
@@ -4,11 +4,14 @@ extends TextureButton
 @export var clickable_sprite : Texture
 @export var scene_path : String
 
+@onready var room = get_tree().get_root().get_node("Room")
+
 func _ready():
 	texture_normal = clickable_sprite
 
 func _on_pressed():
-	var new_scene = load(scene_path)
-	get_tree().change_scene_to_packed(new_scene)
-	
-	
+	if room.state == "active":
+		room.state = "inactive"
+		var scene = load(scene_path)
+		var scene_inst = scene.instantiate()
+		room.add_child(scene_inst)
--- project/Scenes/Pair.gd
+++ project/Scenes/Pair.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var parent = get_tree().get_root().get_node("MiniGame")
+@onready var parent = get_tree().get_root().get_node("Room/MiniGame")
 @export var card1: Sprite2D
 @export var card2: Sprite2D
 
