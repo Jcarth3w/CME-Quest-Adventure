d0aee2f5929fe3b7f41af541b4b4d8e427b6085e
Jack Carthew <jdcarthew@bsu.edu>
2023-11-09 07:39:10 -0800

    Add final room functionality

--- project/Scenes/inventory.gd
+++ project/Scenes/inventory.gd
@@ -15,8 +15,14 @@ func remove_item(item_name):
 		$Item1.texture = null
 	elif x == 1:
 		$Item2.texture = null
-	else:
+	elif x == 2:
 		$Item3.texture = null
+	elif x == 3:
+		$Item4.texture == null
+	elif x == 4:
+		$Item5.texture == null
+	else:
+		$Item6.texture = null
 	items.erase(item_name)
 	
 
@@ -25,6 +31,12 @@ func add_item_image(sprite_path):
 		$Item1.texture = sprite_path
 	elif current_item == 2:
 		$Item2.texture = sprite_path
-	else:
+	elif current_item == 3:
 		$Item3.texture = sprite_path
+	elif current_item == 4:
+		$Item4.texture = sprite_path
+	elif current_item == 5:
+		$Item5.texture = sprite_path
+	else:
+		$Item6.texture = sprite_path
 	current_item += 1
