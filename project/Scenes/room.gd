3c526292e3f7552f133a62a3be3ab59fdcfd3a90
Micah Harker <micah.harker@gmail.com>
2023-11-12 19:32:28 -0500

    Create Menu for Room
    Menu lets you quit game if you want

--- project/Scenes/room.gd
+++ project/Scenes/room.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var menu = preload("res://Scenes/in_game_menu.tscn")
 var state := ""
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,3 +13,19 @@ func _check_win():
 		
 func _process(_delta):
 	_check_win()
+	
+	if Input.is_action_just_pressed("menu"):
+		var menu_inst = menu.instantiate()
+		add_child(menu_inst)
+		menu_inst.pause.connect(_on_menu_pause)
+		menu_inst.resume.connect(_on_menu_resume)
+	
+
+func _on_menu_pause():
+	$HUD/Timer.stop()
+	state = "inactive"
+
+
+func _on_menu_resume():
+	$HUD/Timer.start()
+	state = "active"
