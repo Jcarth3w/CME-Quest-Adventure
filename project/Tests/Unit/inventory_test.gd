b1841ce0da3e92d3c0523ac211535fd95abdad56
Jcarth3w <jdcarthew@bsu.edu>
2023-11-20 19:39:58 -0500

    Add first tests

---
+++ project/Tests/Unit/inventory_test.gd
@@ -0,0 +1,22 @@
+extends GutTest
+
+var test_room_preload = preload("res://Tests/TestRoom.tscn")
+var test_room: Node2D
+var inventory
+var texture
+
+func before_all() -> void:
+	test_room = test_room_preload.instantiate()
+	get_tree().root.add_child(test_room)
+	inventory = test_room.get_node("Inventory")
+	texture = load("res://Assets/Sprites/generic_items/genericItem_color_001.png")
+
+
+func test_add_item_image():
+	inventory.add_item_image(texture)
+	assert_eq(inventory.get_node("Item1").texture, texture)
+	
+func test_add_item_name():
+	inventory.add_item("mike jor", texture)
+	assert_eq(inventory.items[0], "mike jor")
+	
