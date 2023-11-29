a259f5d6258484c9159f2a501fab6b0fb57e8e10
Jared Bowman <jared.bowman@bsu.edu>
2023-11-28 19:41:50 -0500

    Create export functionality for table viewer

--- webserver/index.php
+++ webserver/index.php
@@ -3,11 +3,13 @@
     <body>
         <h1>CME Quest Adventures Database</h1>
         <form method="POST">
+			<label for="scenario">Enter Scenario #:</label>
+            <input type='text' name="scenario"/>
             <label for="findname">Enter Username:</label>
             <input type='text' name="findname"/>
             <input type='submit' value="Search"/>
         </form>
         <?php require("datatable.php")?>
-        <p>You are viewing a read-only table.</p>
+        <p>You are viewing a read-only table. <a href="exporttable.php">Export Table</p>
     </body>
 </html>
