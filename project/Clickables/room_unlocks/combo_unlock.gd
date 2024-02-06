2a53131acb953a3893c499b23b7f819a4c31b46d
Micah Harker <micah.harker@gmail.com>
2024-02-05 20:26:06 -0500

    Finish Iteration 3

--- project/Clickables/room_unlocks/combo_unlock.gd
+++ project/Clickables/room_unlocks/combo_unlock.gd
@@ -20,12 +20,15 @@ func _on_button_press(button) -> void:
 		finished.emit()
 		queue_free()
 	else:
-		$Display.text += button.text
+		if $Display.text == "incorrect":
+			$Display.text = button.text
+		else:
+			$Display.text += button.text
 	
 
 func check_answer(player_answer) -> void:
 	if player_answer == answer:
-		$Display.text = "unlocked"
+		$Display.text = "unlocked room 3"
 		get_parent().unlock(room_number)
 		finished.emit()
 		await get_tree().create_timer(0.5).timeout
