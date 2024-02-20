d629ed64f7747c2ff2b9ca9d7a083456852a8999
Micah Harker <micah.harker@gmail.com>
2024-02-20 10:39:03 -0500

    Make all clickables pop when hovered

--- project/Clickables/clickable.gd
+++ project/Clickables/clickable.gd
@@ -5,3 +5,17 @@ extends TextureButton
 
 func action() -> void:
 	pass
+
+
+func connect_mouse() -> void:
+	set_mouse_filter(0)
+	mouse_entered.connect(on_mouse_entered)
+	mouse_exited.connect(on_mouse_exited)
+
+
+func on_mouse_entered():
+	scale = scale * 1.1
+
+
+func on_mouse_exited():
+	scale = scale / 1.1
