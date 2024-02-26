ca3f4c7c1d6b2ef265298b99f43f87a89b928799
hooperethan123 <emhooper@bsu.edu>
2024-02-26 18:26:37 -0500

    Made Functionality for left and right buttons

--- project/Scenes/main_menus/Scenario_Select.gd
+++ project/Scenes/main_menus/Scenario_Select.gd
@@ -1,6 +1,34 @@
 extends Node2D
 
 var scenario_one = "res://Scenes/scenarios/scenario_1.tscn"
+var scenario_two = "res://Scenes/scenarios/scenario_2.tscn"
+var current_scene = 1
+
 
 func _on_room_one_button_pressed():
 	get_tree().change_scene_to_file(scenario_one)
+
+func _on_room_two_button_pressed():
+	get_tree().change_scene_to_file(scenario_two)
+
+func scene_change():
+	if current_scene == 1:
+		$Scenario1.visible = true
+		$Scenario
+
+
+func _on_left_button_pressed():
+	if  current_scene == 2:
+		current_scene = 1
+		$Scenario1.visible = true
+		$Scenario2.visible = false
+
+
+func _on_right_button_pressed():
+	if  current_scene == 1:
+		current_scene = 2
+		$Scenario1.visible = false
+		$Scenario2.visible = true
+
+
+
