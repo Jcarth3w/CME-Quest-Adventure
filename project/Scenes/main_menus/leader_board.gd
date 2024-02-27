1be46c382dd0a8f794f1c608d3aa43df6b8b0b24
Micah Harker <micah.harker@gmail.com>
2024-02-26 20:46:13 -0500

    Protect against index error

--- project/Scenes/main_menus/leader_board.gd
+++ project/Scenes/main_menus/leader_board.gd
@@ -30,6 +30,7 @@ func on_scenario_pressed(button) -> void:
 
 func _on_http_request_request_completed(result, response_code, headers, body):
 	var raw_string = body.get_string_from_utf8()
+	print(raw_string)
 	parseTimes(raw_string)
 
 
@@ -40,22 +41,22 @@ func parseTimes(raw_string):
 		var user_value = s["username"]
 		times.append(time_value)
 		users.append(user_value)
-		print(times)
 
 
 func populate_leader_board(scenario_number):
 	if scenario_number == 1:
+		var i = 0;
 		for child in $Scenario1Times.get_children():
-			var i = 0;
-			if child.has_node("Username") and child.has_node("Time"):
-				print(times[i])
-				child.get_node("Username").text = users[i]
-				child.get_node("Time").text =  times[i]
-				i = i+1
+			if i < times.size():
+				if child.has_node("Username") and child.has_node("Time"):
+					child.get_node("Username").text = users[i]
+					child.get_node("Time").text =  times[i]
+					i = i+1
 	elif scenario_number == 2:
+		var i = 0;
 		for child in $Scenario2Times.get_children():
-			var i = 0;
-			if child.has_node("Username") and child.has_node("Time"):
-				child.get_node("Username").text = users[i]
-				child.get_node("Time").text =  times[i]
-				i = i+1
+			if i < times.size():
+				if child.has_node("Username") and child.has_node("Time"):
+					child.get_node("Username").text = users[i]
+					child.get_node("Time").text =  times[i]
+					i = i+1
