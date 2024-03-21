169416a2b4fe71b87885c5be60793519027c37a0
Jared Bowman <jaredbruiser@hotmail.com>
2024-03-21 19:55:26 -0400

    Configure post script and .env to login to database

--- project/Components/post_score.gd
+++ project/Components/post_score.gd
@@ -1,6 +1,11 @@
 extends Node
 
 
+func _ready():
+	Firebase.Auth.login_succeeded.connect(on_login_success)
+	Firebase.Auth.login_failed.connect(on_login_failure)
+	Firebase.Auth.login_with_email_and_password("jaredbowman@piesrgr8.com", "jfkgotshotbro")
+
 func make_post_request(scenario, time, username, finished):
 	var url = "http://localhost:8000/postdata.php"
 	var json_object = {
@@ -26,3 +31,10 @@ func make_get_request():
 	# Add 'Content-Type' header:
 	var headers = ["Content-Type: application/json"]
 	$HTTPRequest.request(url, headers, HTTPClient.METHOD_GET)
+
+func on_login_success(auth):
+	print("Login to Firebase was successful!")
+
+func on_login_failure(error_code, message):
+	print(error_code)
+	print(message)
