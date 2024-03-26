86056a4bf2c82f983a3ff9c7b9644bfcdf035689
Jared Bowman <jaredbruiser@hotmail.com>
2024-03-25 20:48:16 -0400

    Change authentication method to anonymous

--- project/Components/post_score.gd
+++ project/Components/post_score.gd
@@ -1,24 +1,16 @@
 extends Node
 
 
-func _ready():
-	Firebase.Auth.login_succeeded.connect(on_login_success)
-	Firebase.Auth.login_failed.connect(on_login_failure)
-	Firebase.Auth.login_with_email_and_password("jaredbowman@piesrgr8.com", "jfkgotshotbro")
-
 func make_post_request(scenario, time, username, finished):
-	var url = "http://localhost:8000/postdata.php"
-	var json_object = {
+	var collection: FirestoreCollection = Firebase.Firestore.collection("scores")
+	var data: Dictionary = {
 		"scenario": scenario,
 		"time": time,
 		"username": username,
-		"finished": finished
+		"finished": finished,
+		"createdAt": Time.get_datetime_string_from_system()
 	}
-	# Convert data to json string:
-	var data = JSON.stringify(json_object)
-	# Add 'Content-Type' header:
-	var headers = ["Content-Type: application/json"]
-	$HTTPRequest.request(url, headers, HTTPClient.METHOD_POST, data)
+	var task: FirestoreTask = collection.update(username, data)
 
 
 func _on_http_request_request_completed(_result, _response_code, _headers, body):
@@ -32,9 +24,3 @@ func make_get_request():
 	var headers = ["Content-Type: application/json"]
 	$HTTPRequest.request(url, headers, HTTPClient.METHOD_GET)
 
-func on_login_success(auth):
-	print("Login to Firebase was successful!")
-
-func on_login_failure(error_code, message):
-	print(error_code)
-	print(message)
