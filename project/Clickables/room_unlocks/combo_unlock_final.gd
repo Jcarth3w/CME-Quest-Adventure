414faac6f66bdca421e2564ff54db47c326cbeef
Micah Harker <micah.harker@gmail.com>
2024-04-14 10:19:27 -0400

    Finish Scenario 2

---
+++ project/Clickables/room_unlocks/combo_unlock_final.gd
@@ -0,0 +1,40 @@
+extends Node2D
+
+signal finished
+var answer = "968211"
+var room_number = 3
+
+# Called when the node enters the scene tree for the first time.
+func _ready() ->void:
+	for child in get_children():
+		if child is Button:
+			child.pressed.connect(on_button_press.bind(child))
+
+
+func on_button_press(button) -> void:
+	match button.text:
+		"ENTER":
+			check_answer($Display.text)
+		"CLEAR":
+			$Display.text = ""
+		"X":
+			get_parent().get_node("Room3Unlock").on_mouse_exited()
+			queue_free()
+			get_parent().resume_room()
+		_:
+			if $Display.text == "incorrect":
+				$Display.text = button.text
+			else:
+				$Display.text += button.text
+
+
+func check_answer(player_answer) -> void:
+	if player_answer == answer:
+		$AudioStreamPlayer.play()
+		$Display.text = "unlocked room 3"
+		get_parent().unlock(room_number)
+		finished.emit()
+		await get_tree().create_timer(0.5).timeout
+		queue_free()
+	else:
+		$Display.text = "incorrect"
---
+++ project/Scenes/mini_games/blooms_tax/blooms_top.gd
@@ -0,0 +1,11 @@
+extends Drag
+
+func create_preview():
+	var preview_rect = TextureRect.new()
+	preview_rect.texture = texture
+	preview_rect.expand_mode = 1
+	preview_rect.size = Vector2(80, 80)
+	preview_rect.position = Vector2(-100, -100)
+	var preview = Control.new()
+	preview.add_child(preview_rect)
+	return preview
