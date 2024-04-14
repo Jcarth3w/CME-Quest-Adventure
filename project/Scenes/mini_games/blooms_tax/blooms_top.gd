2410c7942b4e80257011d9680c723e0c46c27dd4
Micah Harker <micah.harker@gmail.com>
2024-04-14 10:32:28 -0400

    Add to final hint/Fix pyramid top color

--- project/Scenes/mini_games/blooms_tax/blooms_top.gd
+++ project/Scenes/mini_games/blooms_tax/blooms_top.gd
@@ -5,6 +5,7 @@ func create_preview():
 	preview_rect.texture = texture
 	preview_rect.expand_mode = 1
 	preview_rect.size = Vector2(80, 80)
+	preview_rect.modulate = Color(0.09, 1, 1)
 	preview_rect.position = Vector2(-100, -100)
 	var preview = Control.new()
 	preview.add_child(preview_rect)
